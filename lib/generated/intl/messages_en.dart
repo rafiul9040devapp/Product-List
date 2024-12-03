7b29ef9de28bdb81608176d852534ec43e1e8be8
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-12-03 22:21:08 +0600

    The Whole project is completed

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -21,5 +21,41 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "app_name": MessageLookupByLibrary.simpleMessage("CRUD"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "confirm": MessageLookupByLibrary.simpleMessage("Confirm"),
+        "create": MessageLookupByLibrary.simpleMessage("Create"),
+        "create_product":
+            MessageLookupByLibrary.simpleMessage("Create Product"),
+        "delete_confirm":
+            MessageLookupByLibrary.simpleMessage("Delete Confirmation"),
+        "enter_products_name":
+            MessageLookupByLibrary.simpleMessage("Enter Product Name"),
+        "enter_products_quantity":
+            MessageLookupByLibrary.simpleMessage("Enter Product Quantity"),
+        "enter_products_unit_price":
+            MessageLookupByLibrary.simpleMessage("Enter Product Unit Price"),
+        "home": MessageLookupByLibrary.simpleMessage("Home"),
+        "no_name": MessageLookupByLibrary.simpleMessage("No Name"),
+        "no_value_updated":
+            MessageLookupByLibrary.simpleMessage("No value is updated"),
+        "not_available": MessageLookupByLibrary.simpleMessage("N/A"),
+        "not_internet_connection": MessageLookupByLibrary.simpleMessage(
+            "No Internet Connection.\nPlease check your network settings."),
+        "page_not_found":
+            MessageLookupByLibrary.simpleMessage("Page not found"),
+        "products": MessageLookupByLibrary.simpleMessage("Products"),
+        "products_code": MessageLookupByLibrary.simpleMessage("Product Code"),
+        "products_name": MessageLookupByLibrary.simpleMessage("Product Name"),
+        "products_quantity":
+            MessageLookupByLibrary.simpleMessage("Product Quantity"),
+        "products_unit_price":
+            MessageLookupByLibrary.simpleMessage("Product Unit Price"),
+        "quantity": MessageLookupByLibrary.simpleMessage("Quantity"),
+        "total_price": MessageLookupByLibrary.simpleMessage("Total Price"),
+        "unit_price": MessageLookupByLibrary.simpleMessage("Unit Price"),
+        "update": MessageLookupByLibrary.simpleMessage("Update"),
+        "update_product": MessageLookupByLibrary.simpleMessage("Update Product")
+      };
 }
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -49,6 +49,256 @@ class S {
   static S? maybeOf(BuildContext context) {
     return Localizations.of<S>(context, S);
   }
+
+  /// `CRUD`
+  String get app_name {
+    return Intl.message(
+      'CRUD',
+      name: 'app_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Products`
+  String get products {
+    return Intl.message(
+      'Products',
+      name: 'products',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Product Code`
+  String get products_code {
+    return Intl.message(
+      'Product Code',
+      name: 'products_code',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Product Name`
+  String get products_name {
+    return Intl.message(
+      'Product Name',
+      name: 'products_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter Product Name`
+  String get enter_products_name {
+    return Intl.message(
+      'Enter Product Name',
+      name: 'enter_products_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Product Quantity`
+  String get products_quantity {
+    return Intl.message(
+      'Product Quantity',
+      name: 'products_quantity',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter Product Quantity`
+  String get enter_products_quantity {
+    return Intl.message(
+      'Enter Product Quantity',
+      name: 'enter_products_quantity',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Product Unit Price`
+  String get products_unit_price {
+    return Intl.message(
+      'Product Unit Price',
+      name: 'products_unit_price',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Enter Product Unit Price`
+  String get enter_products_unit_price {
+    return Intl.message(
+      'Enter Product Unit Price',
+      name: 'enter_products_unit_price',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Quantity`
+  String get quantity {
+    return Intl.message(
+      'Quantity',
+      name: 'quantity',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Unit Price`
+  String get unit_price {
+    return Intl.message(
+      'Unit Price',
+      name: 'unit_price',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Total Price`
+  String get total_price {
+    return Intl.message(
+      'Total Price',
+      name: 'total_price',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `N/A`
+  String get not_available {
+    return Intl.message(
+      'N/A',
+      name: 'not_available',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `No Internet Connection.\nPlease check your network settings.`
+  String get not_internet_connection {
+    return Intl.message(
+      'No Internet Connection.\nPlease check your network settings.',
+      name: 'not_internet_connection',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Home`
+  String get home {
+    return Intl.message(
+      'Home',
+      name: 'home',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Create`
+  String get create {
+    return Intl.message(
+      'Create',
+      name: 'create',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Update`
+  String get update {
+    return Intl.message(
+      'Update',
+      name: 'update',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Create Product`
+  String get create_product {
+    return Intl.message(
+      'Create Product',
+      name: 'create_product',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Update Product`
+  String get update_product {
+    return Intl.message(
+      'Update Product',
+      name: 'update_product',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Delete Confirmation`
+  String get delete_confirm {
+    return Intl.message(
+      'Delete Confirmation',
+      name: 'delete_confirm',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cancel`
+  String get cancel {
+    return Intl.message(
+      'Cancel',
+      name: 'cancel',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Confirm`
+  String get confirm {
+    return Intl.message(
+      'Confirm',
+      name: 'confirm',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `No Name`
+  String get no_name {
+    return Intl.message(
+      'No Name',
+      name: 'no_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Page not found`
+  String get page_not_found {
+    return Intl.message(
+      'Page not found',
+      name: 'page_not_found',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `No value is updated`
+  String get no_value_updated {
+    return Intl.message(
+      'No value is updated',
+      name: 'no_value_updated',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
